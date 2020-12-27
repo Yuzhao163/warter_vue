f101f74c8aaa45c8f758e5294a603f6fbe54ae6a
OctopusYX8 <57257747+OctopusYX8@users.noreply.github.com>
2020-12-27 17:38:04 +0800

    更新个人信息

--- src/components/home/UserMessage.vue
+++ src/components/home/UserMessage.vue
@@ -1,13 +1,175 @@
 <template>
-  <div></div>
+  <div class="mainbox">
+    <div class="top">
+      <div class="title">
+        个人信息修改
+      </div>
+      <div class="userid">用户ID--{{ this.message.UserID }}</div>
+    </div>
+    <div class="linetext">
+      <div style="width: 150px;line-height:40px;margin-left: 10px">用户名：</div>
+      <el-input ref="inputunm" v-model="message.UserName" :disabled="this.disabled.disabledunm"></el-input>
+      <div style="width: 100px;line-height:40px;">
+        <el-button type="primary" size="small" round @click="modifyunm">修改</el-button>
+      </div>
+    </div>
+    <div class="linetext">
+      <div style="width: 150px;line-height:40px;margin-left: 10px">密码：</div>
+      <el-input ref="inputpsw" v-model="message.UserPswd" :disabled="this.disabled.disabledpsw"
+                show-password></el-input>
+      <div style="width: 100px;line-height:40px;">
+        <el-button type="primary" size="small" round @click="modifypsw">修改</el-button>
+      </div>
+    </div>
+    <div class="linetext">
+      <div style="width: 150px;line-height:40px;margin-left: 10px">真实姓名：</div>
+      <el-input ref="inputunm" v-model="message.RealName" :disabled="this.disabled.disabledrn"></el-input>
+      <div style="width: 100px;line-height:40px;">
+        <el-button type="primary" size="small" round @click="modifyrn">修改</el-button>
+      </div>
+    </div>
+    <div class="linetext">
+      <div style="width: 150px;line-height:40px;margin-left: 10px">联系电话：</div>
+      <el-input ref="inputunm" v-model="message.MoPhone" :disabled="this.disabled.disabledunm"></el-input>
+      <div style="width: 100px;line-height:40px;">
+        <el-button type="primary" size="small" round @click="modifyph">修改</el-button>
+      </div>
+    </div>
+    <div class="linetext">
+      <div style="width: 150px;line-height:40px;margin-left: 10px">单位名称：</div>
+      <el-input ref="inputunm" v-model="message.DPTName" :disabled="this.disabled.disableddpt"></el-input>
+      <div style="width: 100px;line-height:40px;">
+        <el-button type="primary" size="small" round @click="modifydpt">修改</el-button>
+      </div>
+    </div>
+    <div class="boxfoot">
+      <el-button type="primary" plain round @click="confirm">提交</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "UserMessage"
+  name: "UserMessage",
+  data() {
+    return {
+      disabled: {
+        disabledunm: true,
+        disabledpsw: true,
+        disabledrn: true,
+        disabledph: true,
+        disableddpt: true,
+      },
+      message: {
+        UserID: "10001",
+        UserName: 'zhangsan001',
+        UserPswd: '123456',
+        UClassName: '管线管理',
+        MoPhone: '18832109876',
+        RealName: '张三',
+        DPTName: '排水集团',
+      }
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    modifyunm() {
+      if (this.disabled.disabledunm == true) {
+        this.disabled.disabledunm = false;
+      } else {
+        this.disabled.disabledunm = true;
+      }
+    },
+    modifypsw() {
+      if (this.disabled.disabledpsw == true) {
+        this.disabled.disabledpsw = false;
+      } else {
+        this.disabled.disabledpsw = true;
+      }
+    },
+    modifyrn() {
+      if (this.disabled.disabledrn == true) {
+        this.disabled.disabledrn = false;
+      } else {
+        this.disabled.disabledrn = true;
+      }
+    },
+    modifyph() {
+      if (this.disabled.disabledph == true) {
+        this.disabled.disabledph = false;
+      } else {
+        this.disabled.disabledph = true;
+      }
+    },
+    modifydpt() {
+      if (this.disabled.disableddpt == true) {
+        this.disabled.disableddpt = false;
+      } else {
+        this.disabled.disableddpt = true;
+      }
+    },
+    confirm() {
+      this.$confirm('确定要修改您的信息？(请牢记您的密码)', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '修改成功'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消修改'
+        });
+      });
+    }
+  }
 }
 </script>
 
 <style scoped>
+.mainbox {
+  width: 600px;
+  border-radius: 20px;
+  background-color: #ffffff;
+  margin: 0 auto;
+  overflow: hidden
+}
+
+.top {
+
+
+}
+
+.title {
+  display: flex;
+  font-size: 24px;
+  font-weight: bolder;
+  margin: 10px 0 10px 10px;
+}
+
+.userid {
+  size: 12px;
+  display: flex;
+  color: #616180;
+  font-weight: bolder;
+  margin: 10px 0 10px 15px;
+}
+
+.linetext {
+  display: flex;
+  margin: 20px 0;
+}
+.boxfoot{
+  margin: 20px;
+}
+
+/deep/ .el-input.is-disabled .el-input__inner {
+  color: black;
+}
 
 </style>
